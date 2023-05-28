31be004f6addc8ced3a8b59e4e990afebc14e4aa
Chanthorn Yoeun <chanthorn.developer@gmail.com>
2023-05-28 15:24:23 +0700

    Show connection message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'connection-status';
+
+  connentionMessage: string;
+
+  constructor() {
+    this.connentionMessage = navigator.onLine ? 'You are connected to the internet.' : 'You are not connected to the internet.';
+  }
+
+  @HostListener('window:online')
+  private online() {
+    this.connentionMessage = 'You are connected to the internet.';
+  }
+
+  @HostListener('window:offline')
+  private offline() {
+    this.connentionMessage = 'You are not connected to the internet.';
+  }
+
 }
